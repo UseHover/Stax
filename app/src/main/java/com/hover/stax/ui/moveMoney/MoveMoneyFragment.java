95e3f26fb9ec39a4ad147fbea45ff1a62d42d9d8
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2020-08-17 15:29:34 +0100

    setting up the move money items activities

--- app/src/main/java/com/hover/stax/ui/moveMoney/MoveMoneyFragment.java
+++ app/src/main/java/com/hover/stax/ui/moveMoney/MoveMoneyFragment.java
@@ -1,5 +1,6 @@
 package com.hover.stax.ui.moveMoney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,14 +15,25 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.hover.stax.R;
+import com.hover.stax.ui.moveMoney.betweenServices.BetweenServicesActivity;
+import com.hover.stax.ui.moveMoney.requestMoney.RequestMoneyActivity;
+import com.hover.stax.ui.moveMoney.toSomeoneEsle.ToSomeElseActivity;
 
 public class MoveMoneyFragment extends Fragment {
 
-private MoveMoneyViewModel moveMoneyViewModel;
-
 public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	View root = inflater.inflate(R.layout.fragment_movemoney, container, false);
+	root.findViewById(R.id.betweenServicesText).setOnClickListener(view-> {
+	startActivity(new Intent(getActivity(), BetweenServicesActivity.class));
+	});
 
+	root.findViewById(R.id.toSomeElseText).setOnClickListener(view-> {
+	startActivity(new Intent(getActivity(), ToSomeElseActivity.class));
+	});
+
+	root.findViewById(R.id.requestMoneyText).setOnClickListener(view-> {
+	startActivity(new Intent(getActivity(), RequestMoneyActivity.class));
+	});
 
 	return root;
 }
---
+++ app/src/main/java/com/hover/stax/ui/moveMoney/betweenServices/BetweenServicesActivity.java
@@ -0,0 +1,13 @@
+package com.hover.stax.ui.moveMoney.betweenServices;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BetweenServicesActivity extends AppCompatActivity {
+@Override
+protected void onCreate(@Nullable Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+}
+}
