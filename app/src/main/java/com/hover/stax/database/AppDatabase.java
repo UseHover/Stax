28e5dc9abfeaa69123b5eb36fa88a365c5fc6ff8
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2020-09-09 00:47:54 +0100

    removed quering db on main thread

--- app/src/main/java/com/hover/stax/database/AppDatabase.java
+++ app/src/main/java/com/hover/stax/database/AppDatabase.java
@@ -27,7 +27,6 @@ public abstract class AppDatabase extends RoomDatabase {
 			synchronized (AppDatabase.class) {
 				if (INSTANCE == null) {
 					INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "stax.db")
-									   .allowMainThreadQueries()
 					   .fallbackToDestructiveMigration()
 			           .build();
 				}
