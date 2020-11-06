50ec252637bc25f27166ef0c3d7d7bf56ed9e05e
kutalek <davkutalek@gmail.com>
2020-11-05 20:58:44 -0800

    Increased appdb version to fix updagrade issues

--- app/src/main/java/com/hover/stax/database/AppDatabase.java
+++ app/src/main/java/com/hover/stax/database/AppDatabase.java
@@ -18,7 +18,7 @@ import com.hover.stax.transactions.TransactionDao;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Channel.class, StaxTransaction.class, Request.class, Schedule.class}, version = 12)
+@Database(entities = {Channel.class, StaxTransaction.class, Request.class, Schedule.class}, version = 13)
 public abstract class AppDatabase extends RoomDatabase {
 	private static final int NUMBER_OF_THREADS = 8;
 	static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
