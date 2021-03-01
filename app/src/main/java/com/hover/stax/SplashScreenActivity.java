4c07907d1c58c71c3c688bd5631061f33645a662
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-03-01 22:35:11 +0100

    minor naming changes

--- app/src/main/java/com/hover/stax/SplashScreenActivity.java
+++ app/src/main/java/com/hover/stax/SplashScreenActivity.java
@@ -43,7 +43,7 @@ public class SplashScreenActivity extends AppCompatActivity implements Biometric
 
 		if(shouldSelfDestructWhenAppVersionExpires(false)) {return;}
 		startBackgroundProcesses();
-		startSplashForegroundProcesses();
+		startSplashForegroundSequence();
 
 	}
 
@@ -53,7 +53,7 @@ public class SplashScreenActivity extends AppCompatActivity implements Biometric
 		createNotificationChannel();
 		startWorkers();
 	}
-	private void startSplashForegroundProcesses() {
+	private void startSplashForegroundSequence() {
 		initSplashAnimation();
 		authenticateBiometricOrNavigateScreenAfter120sec();
 	}
