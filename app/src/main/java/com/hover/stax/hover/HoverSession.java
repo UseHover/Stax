52d3a81e0fb80b6737352adaa669a2728c7e907e
kutalek <davkutalek@gmail.com>
2021-03-02 13:40:34 -0800

    Updated tagline, fixed missing processing screen background image

--- app/src/main/java/com/hover/stax/hover/HoverSession.java
+++ app/src/main/java/com/hover/stax/hover/HoverSession.java
@@ -50,6 +50,7 @@ final public class HoverSession {
 		builder.showUserStepDescriptions(true);
 		builder.finalMsgDisplayTime(finalScreenTime);
 		builder.style(R.style.StaxHoverTheme);
+		builder.customBackgroundImage(R.drawable.stax_background);
 		return builder;
 	}
 
