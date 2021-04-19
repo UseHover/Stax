19fb515a67eca7193feb2f5760bbf9ce970f1a6d
Alex Kombo <alexkombo@gmail.com>
2021-04-19 15:52:45 +0300

    refactors channels

--- app/src/main/java/com/hover/stax/views/StaxCardView.java
+++ app/src/main/java/com/hover/stax/views/StaxCardView.java
@@ -9,28 +9,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.hover.stax.R;
+import com.hover.stax.databinding.StaxCardViewBinding;
 
 public class StaxCardView extends FrameLayout {
 
 	private String title;
 	private boolean showBack, useContextBackPress;
 
-	private LinearLayout contentView;
-	public ImageButton backButton;
 	private int backDrawable = 0;
 	private int bgColor;
+
+	private final StaxCardViewBinding binding;
 
 	public StaxCardView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		getAttrs(context, attrs);
-		LayoutInflater.from(context).inflate(R.layout.stax_card_view, this);
-		contentView = findViewById(R.id.content);
-		backButton = findViewById(R.id.backButton);
+
+		binding = StaxCardViewBinding.inflate(LayoutInflater.from(context), this, true);
 		fillFromAttrs();
 	}
 
@@ -50,41 +47,41 @@ public class StaxCardView extends FrameLayout {
 	@SuppressLint("ResourceType")
 	public void setBackgroundColor(int colorRes) {
 		bgColor = getContext().getResources().getColor(colorRes);
-		contentView.setBackgroundColor(bgColor);
+		binding.content.setBackgroundColor(bgColor);
 	}
 
 	public void setTitle(String t) {
-		if (t != null) ((TextView) findViewById(R.id.title)).setText(t);
+		if (t != null) binding.title.setText(t);
 	}
 
 	public void setTitle(int titleString) {
-		if (titleString != 0) ((TextView) findViewById(R.id.title)).setText(getContext().getString(titleString));
+		if (titleString != 0) binding.title.setText(getContext().getString(titleString));
 	}
 
 	public void setIcon(int icon) {
-		if (icon != 0) { backButton.setImageResource(icon); }
+		if (icon != 0) { binding.backButton.setImageResource(icon); }
 	}
 
 	public void setOnClickIcon(OnClickListener listener) {
-		if (listener != null) { backButton.setOnClickListener(listener); }
+		if (listener != null) { binding.backButton.setOnClickListener(listener); }
 	}
 
 	private void fillFromAttrs() {
-		if (title != null) ((TextView) findViewById(R.id.title)).setText(title);
+		if (title != null) binding.title.setText(title);
 		else findViewById(R.id.header).setVisibility(GONE);
 
-		if (useContextBackPress) backButton.setOnClickListener(view -> triggerBack());
-		if (showBack) backButton.setVisibility(VISIBLE);
-		if (backDrawable != 0) backButton.setImageResource(backDrawable);
-		contentView.setBackgroundColor(bgColor);
+		if (useContextBackPress) binding.backButton.setOnClickListener(view -> triggerBack());
+		if (showBack) binding.backButton.setVisibility(VISIBLE);
+		if (backDrawable != 0) binding.backButton.setImageResource(backDrawable);
+		binding.content.setBackgroundColor(bgColor);
 	}
 
 	@Override
 	public void addView(View child, int index, ViewGroup.LayoutParams params) {
-		if (contentView == null)
+		if (binding.content == null)
 			super.addView(child, index, params);
 		else
-			contentView.addView(child, index, params);
+			binding.content.addView(child, index, params);
 	}
 
 	private void triggerBack() {
