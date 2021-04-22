cca3fe3025d5905f0e294be04c2ce3f2e7a6b81f
Alex Kombo <alexkombo@gmail.com>
2021-04-22 15:01:04 +0300

    adds imeOptions to text input layouts

--- app/src/main/java/com/hover/stax/views/StaxTextInputLayout.java
+++ app/src/main/java/com/hover/stax/views/StaxTextInputLayout.java
@@ -16,53 +16,63 @@ import com.hover.stax.databinding.StaxInputBinding;
 public class StaxTextInputLayout extends AbstractStatefulInput {
     private final String TAG = "StaxTextInputLayout";
 
-	private String hint;
-	private int inputType;
-	private TextInputEditText editText;
+    private String hint;
+    private int inputType;
+    private int imeOptions;
 
-	private StaxInputBinding binding;
+    private TextInputEditText editText;
 
-	public StaxTextInputLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-		super(context, attrs);
-		getAttrs(context,attrs);
+    private StaxInputBinding binding;
 
-		binding = StaxInputBinding.inflate(LayoutInflater.from(context), this, true);
+    public StaxTextInputLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        getAttrs(context, attrs);
+
+        binding = StaxInputBinding.inflate(LayoutInflater.from(context), this, true);
 
 //		inflate(context, R.layout.stax_input, this);
-		initView();
-		fillAttr();
-	}
+        initView();
+        fillAttr();
+    }
 
-	protected void initView() {
-		super.initView();
-		editText = binding.inputEditText;
-	}
+    protected void initView() {
+        super.initView();
+        editText = binding.inputEditText;
+    }
 
-	private void getAttrs(Context context, AttributeSet attrs) {
-		TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.StaxTextInputLayout, 0, 0);
-		try {
-			hint = a.getString(R.styleable.StaxTextInputLayout_android_hint);
-			inputType = a.getInt(R.styleable.StaxTextInputLayout_android_inputType, 0);
-		} finally {
-			a.recycle();
-		}
-	}
+    private void getAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.StaxTextInputLayout, 0, 0);
+        try {
+            hint = a.getString(R.styleable.StaxTextInputLayout_android_hint);
+            inputType = a.getInt(R.styleable.StaxTextInputLayout_android_inputType, 0);
+            imeOptions = a.getInt(R.styleable.StaxTextInputLayout_android_imeOptions, 0);
+        } finally {
+            a.recycle();
+        }
+    }
 
-	private void fillAttr() {
-		if (hint != null) binding.inputEditText.setHint(hint);
-		if (inputType > 0) binding.inputEditText.setInputType(inputType);
-	}
+    private void fillAttr() {
+        if (hint != null) binding.inputEditText.setHint(hint);
+        if (inputType > 0) binding.inputEditText.setInputType(inputType);
+        if (imeOptions > 0) binding.inputEditText.setImeOptions(imeOptions);
+    }
 
-	public void setText(String text) {
-		editText.setText(text);
-		if (text != null && !text.isEmpty())
-			setState(null, SUCCESS);
-	}
+    public void setText(String text) {
+        editText.setText(text);
+        if (text != null && !text.isEmpty())
+            setState(null, SUCCESS);
+    }
 
-	public String getText() { return editText.getText().toString(); }
+    public String getText() {
+        return editText.getText().toString();
+    }
 
-	@Override
-	public void setOnFocusChangeListener(OnFocusChangeListener l) { editText.setOnFocusChangeListener(l); }
+    @Override
+    public void setOnFocusChangeListener(OnFocusChangeListener l) {
+        editText.setOnFocusChangeListener(l);
+    }
 
-	public void addTextChangedListener(TextWatcher listener) { editText.addTextChangedListener(listener); }
+    public void addTextChangedListener(TextWatcher listener) {
+        editText.addTextChangedListener(listener);
+    }
 }
