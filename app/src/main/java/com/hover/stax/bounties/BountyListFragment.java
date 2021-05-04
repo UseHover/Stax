943273f478763f6bf8e252d5fe23a23461e2488d
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-05-04 10:47:40 +0100

    support targeting notification by bounty country, and any bounty

--- app/src/main/java/com/hover/stax/bounties/BountyListFragment.java
+++ app/src/main/java/com/hover/stax/bounties/BountyListFragment.java
@@ -20,6 +20,7 @@ import com.hover.stax.countries.CountryAdapter;
 import com.hover.stax.countries.CountryDropdown;
 import com.hover.stax.navigation.NavigationInterface;
 import com.hover.stax.utils.UIHelper;
+import com.hover.stax.utils.Utils;
 import com.hover.stax.views.StaxDialog;
 
 import java.util.List;
@@ -105,6 +106,8 @@ public class BountyListFragment extends Fragment implements NavigationInterface,
 			.setDialogTitle(getString(R.string.bounty_claim_title, b.action.root_code, b.action.getHumanFriendlyType(getContext(), b.action.transaction_type), b.action.bounty_amount))
 			.setDialogMessage(getString(R.string.bounty_claim_explained, b.action.bounty_amount, b.getInstructions(getContext())))
 			.setPosButton(R.string.start_USSD_Flow, v -> {
+					Utils.setFirebaseMessagingTopic(getString(R.string.firebase_topic_yes_bounty_yes_try));
+					Utils.setFirebaseMessagingTopic(getString(R.string.firebase_topic_bounty_in_country, b.action.country_alpha2.toUpperCase()));
 					((BountyActivity) requireActivity()).makeCall(b.action);
 			})
 			.showIt();
