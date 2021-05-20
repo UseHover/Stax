09f8030cbb7660416478f182b07e3a52dab0be6f
Alex Kombo <alexkombo@gmail.com>
2021-05-20 12:17:36 +0300

    adds sentry for logging

--- app/src/main/java/com/hover/stax/ApplicationInstance.kt
+++ app/src/main/java/com/hover/stax/ApplicationInstance.kt
@@ -1,11 +1,15 @@
 package com.hover.stax
 
 import android.app.Application
+import android.content.pm.PackageManager
 import com.google.firebase.FirebaseApp
 import com.hover.stax.di.appModule
 import com.hover.stax.di.dataModule
+import com.hover.stax.utils.Utils
 import com.hover.stax.utils.fonts.FontReplacer
 import com.yariksoffice.lingver.Lingver
+import io.sentry.Sentry
+import io.sentry.android.core.SentryAndroid
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -16,12 +20,13 @@ class ApplicationInstance : Application() {
     override fun onCreate() {
         super.onCreate()
 
-
         setFont()
         initDI()
 
         setLogger()
         FirebaseApp.initializeApp(this)
+
+        initSentry()
     }
 
     private fun setFont() {
@@ -35,10 +40,33 @@ class ApplicationInstance : Application() {
         Lingver.init(this, Locale.getDefault())
     }
 
-    private fun initDI(){
+    private fun initDI() {
         startKoin {
             androidContext(this@ApplicationInstance)
             modules(listOf(appModule, dataModule))
+        }
+    }
+
+    private fun initSentry() {
+        SentryAndroid.init(this) {
+            it.dsn = getString(R.string.sentry_dsn)
+            it.release = BuildConfig.VERSION_NAME
+            it.sessionTrackingIntervalMillis = 60000
+        }
+
+        addTags()
+    }
+
+    private fun addTags() {
+        Sentry.configureScope {
+            it.setTag("app_name", Utils.getPackage(this))
+
+            try {
+                val pInfo = packageManager.getPackageInfo(Utils.getPackage(this), 0)
+                Sentry.setTag("app_version", pInfo.versionName)
+                Sentry.setTag("app_version_code", pInfo.versionCode.toString())
+            } catch (ignored: PackageManager.NameNotFoundException) {
+            }
         }
     }
 
