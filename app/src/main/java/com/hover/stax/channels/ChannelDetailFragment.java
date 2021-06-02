9f3d49e2bb856981fb030be0685a6382ad76741a
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-06-02 17:21:39 +0100

    completed story for: As a user, I want to see my requests in my account details page

--- app/src/main/java/com/hover/stax/channels/ChannelDetailFragment.java
+++ app/src/main/java/com/hover/stax/channels/ChannelDetailFragment.java
@@ -14,24 +14,39 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.hover.sdk.transactions.TransactionContract;
 import com.hover.stax.R;
+import com.hover.stax.balances.FutureViewModel;
+import com.hover.stax.balances.RequestsAdapter;
+import com.hover.stax.balances.ScheduledAdapter;
 import com.hover.stax.databinding.FragmentChannelBinding;
 import com.hover.stax.home.MainActivity;
+import com.hover.stax.navigation.NavigationInterface;
+import com.hover.stax.requests.Request;
+import com.hover.stax.schedules.Schedule;
 import com.hover.stax.transactions.TransactionHistoryAdapter;
 import com.hover.stax.utils.UIHelper;
 import com.hover.stax.utils.Utils;
 
-public class ChannelDetailFragment extends Fragment implements TransactionHistoryAdapter.SelectListener {
+import java.util.List;
 
-    private RecyclerView transactionHistoryRecyclerView;
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public class ChannelDetailFragment extends Fragment implements
+        TransactionHistoryAdapter.SelectListener,
+        ScheduledAdapter.SelectListener,
+        RequestsAdapter.SelectListener,
+        NavigationInterface {
+
     private ChannelDetailViewModel viewModel;
-
+    private FutureViewModel futureViewModel;
     private FragmentChannelBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewModel = new ViewModelProvider(requireActivity()).get(ChannelDetailViewModel.class);
         Utils.logAnalyticsEvent(getString(R.string.visit_screen, getString(R.string.visit_channel)), requireContext());
+        viewModel = new ViewModelProvider(requireActivity()).get(ChannelDetailViewModel.class);
+        futureViewModel = new ViewModelProvider(requireActivity()).get(FutureViewModel.class);
         binding = FragmentChannelBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -39,16 +54,23 @@ public class ChannelDetailFragment extends Fragment implements TransactionHistor
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        transactionHistoryRecyclerView = binding.homeCardTransactions.transactionHistoryRecyclerView;
-
         binding.refreshBalanceBtn.setOnClickListener(view1 -> onRefresh());
+        setupObservers();
 
+        assert getArguments() != null;
+        viewModel.setChannel(getArguments().getInt(TransactionContract.COLUMN_CHANNEL_ID));
+
+    }
+    private void setupObservers() {
         viewModel.getChannel().observe(getViewLifecycleOwner(), channel -> {
             binding.staxCardView.setTitle(channel.name);
             binding.feesDescription.setText(getString(R.string.fees_label, channel.name));
             binding.detailsBalance.setText(channel.latestBalance);
+
+            setUpFuture(channel);
         });
 
+        RecyclerView transactionHistoryRecyclerView = binding.homeCardTransactions.transactionHistoryRecyclerView;
         viewModel.getStaxTransactions().observe(getViewLifecycleOwner(), staxTransactions -> {
             binding.homeCardTransactions.noHistory.setVisibility(staxTransactions == null || staxTransactions.size() == 0 ? View.VISIBLE : View.GONE);
             transactionHistoryRecyclerView.setLayoutManager(UIHelper.setMainLinearManagers(getContext()));
@@ -62,10 +84,28 @@ public class ChannelDetailFragment extends Fragment implements TransactionHistor
         viewModel.getFeesThisYear().observe(getViewLifecycleOwner(), sum -> {
             binding.detailsFees.setText(Utils.formatAmount(sum != null ? sum : 0));
         });
-
-        viewModel.setChannel(getArguments().getInt(TransactionContract.COLUMN_CHANNEL_ID));
     }
 
+    private void setUpFuture(Channel channel) {
+        futureViewModel.getScheduledByChannel(channel.id).observe(getViewLifecycleOwner(), schedules -> {
+            RecyclerView recyclerView = binding.scheduledCard.scheduledRecyclerView;
+            recyclerView.setLayoutManager(UIHelper.setMainLinearManagers(getContext()));
+            recyclerView.setAdapter(new ScheduledAdapter(schedules, this));
+            setFutureVisible(schedules, futureViewModel.getRequests().getValue());
+        });
+
+        futureViewModel.getRequestsByChannel(channel.institutionId).observe(getViewLifecycleOwner(), requests -> {
+            RecyclerView rv = binding.scheduledCard.requestsRecyclerView;
+            rv.setLayoutManager(UIHelper.setMainLinearManagers(getContext()));
+            rv.setAdapter(new RequestsAdapter(requests, this));
+            setFutureVisible(futureViewModel.getScheduled().getValue(), requests);
+        });
+    }
+
+    private void setFutureVisible(List<Schedule> schedules, List<Request> requests) {
+        boolean visible = (schedules != null && schedules.size() > 0) || (requests != null && requests.size() > 0);
+        binding.scheduledCard.getRoot().setVisibility(visible ? VISIBLE : GONE);
+    }
 
     @Override
     public void viewTransactionDetail(String uuid) {
@@ -83,5 +123,15 @@ public class ChannelDetailFragment extends Fragment implements TransactionHistor
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void viewScheduledDetail(int id) {
+        navigateToScheduleDetailsFragment(id, this);
+    }
+
+    @Override
+    public void viewRequestDetail(int id) {
+        navigateToRequestDetailsFragment(id, this);
     }
 }
--- app/src/main/java/com/hover/stax/channels/ChannelDetailViewModel.java
+++ app/src/main/java/com/hover/stax/channels/ChannelDetailViewModel.java
@@ -29,6 +29,7 @@ public class ChannelDetailViewModel extends AndroidViewModel {
         repo = new DatabaseRepo(application);
         id = new MutableLiveData<>();
         channel = Transformations.switchMap(id, id -> repo.getLiveChannel(id));
+
         transactions = Transformations.switchMap(id, id -> repo.getCompleteTransferTransactions(id));
         spentThisMonth = Transformations.switchMap(id, this::loadSpentThisMonth);
         feesThisYear = Transformations.switchMap(id, this::loadFeesThisYear);
