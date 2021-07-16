70df84d16280ef4d212fedc68045c745fe4a2365
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-07-16 13:26:34 +0100

    updated FAQViewHolder to be defined as an inner class to allow for access to it and the listener.

--- app/src/main/java/com/hover/stax/faq/FAQAdapter.kt
+++ app/src/main/java/com/hover/stax/faq/FAQAdapter.kt
@@ -5,21 +5,21 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hover.stax.databinding.FaqItemsBinding
 
-class FAQAdapter(private val faqs: List<FAQ>, private val selectListener: SelectListener):
-        RecyclerView.Adapter<FAQAdapter.Companion.FAQViewHolder>() {
+class FAQAdapter(private val faqs: List<FAQ>, private val selectListener: SelectListener) :
+        RecyclerView.Adapter<FAQAdapter.FAQViewHolder>() {
 
-    companion object {
-        class FAQViewHolder(private val itemBinding: FaqItemsBinding) : RecyclerView.ViewHolder(itemBinding.root) {
-            fun setItem(faq: FAQ, selectListener: SelectListener) {
-                itemBinding.faqTopicItem.text = faq.topic
-                itemBinding.root.setOnClickListener { selectListener.onTopicClicked(faq) }
-            }
-        }
 
-        interface SelectListener {
-            fun onTopicClicked(faq: FAQ)
+    inner class FAQViewHolder(private val itemBinding: FaqItemsBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        fun setItem(faq: FAQ, selectListener: SelectListener) {
+            itemBinding.faqTopicItem.text = faq.topic
+            itemBinding.root.setOnClickListener { selectListener.onTopicClicked(faq) }
         }
     }
+
+    interface SelectListener {
+        fun onTopicClicked(faq: FAQ)
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FAQViewHolder {
         val binding: FaqItemsBinding = FaqItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
