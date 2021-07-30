596125c9903cd3d17aade9fa7d28f27263fd8aca
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-07-30 15:13:54 +0100

    updated bounty email and viewModel to kotlin, and replaced async task to coroutines

---
+++ app/src/main/java/com/hover/stax/bounties/BountyEmailFragment.kt
@@ -0,0 +1,114 @@
+package com.hover.stax.bounties
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.hover.stax.R
+import com.hover.stax.databinding.FragmentBountyEmailBinding
+import com.hover.stax.navigation.NavigationInterface
+import com.hover.stax.utils.Utils.getString
+import com.hover.stax.utils.Utils.logAnalyticsEvent
+import com.hover.stax.utils.Utils.logErrorAndReportToFirebase
+import com.hover.stax.utils.Utils.saveString
+import com.hover.stax.utils.network.NetworkMonitor
+import com.hover.stax.views.AbstractStatefulInput
+import com.hover.stax.views.StaxDialog
+import com.hover.stax.views.StaxTextInputLayout
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import org.koin.java.KoinJavaComponent.get
+
+class BountyEmailFragment : Fragment(), NavigationInterface, View.OnClickListener {
+    private var emailInput: StaxTextInputLayout? = null
+    private var binding: FragmentBountyEmailBinding? = null
+    private var dialog: StaxDialog? = null
+    private val networkMonitor = get(NetworkMonitor::class.java)
+    private val viewModel: BountyViewModel by sharedViewModel()
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBountyEmailBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeEmailResult()
+        emailInput = binding!!.emailInput
+        emailInput!!.text = getString(BountyActivity.EMAIL_KEY, requireActivity())
+        binding!!.continueEmailBountyButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (networkMonitor.isNetworkConnected) {
+            logAnalyticsEvent(getString(R.string.clicked_bounty_email_continue_btn), requireContext())
+            if (validates()) {
+                emailInput!!.isEnabled = false
+                viewModel.uploadBountyUser(emailInput!!.text)
+                emailInput!!.setState(getString(R.string.bounty_uploading_email), AbstractStatefulInput.INFO)
+            } else {
+                emailInput!!.setState(getString(R.string.bounty_email_error), AbstractStatefulInput.ERROR)
+            }
+        } else {
+            showOfflineDialog()
+        }
+    }
+
+    private fun showOfflineDialog() {
+        StaxDialog(requireActivity())
+                .setDialogTitle(R.string.internet_required)
+                .setDialogMessage(R.string.internet_required_bounty_desc)
+                .setPosButton(R.string.btn_ok, null)
+                .makeSticky()
+                .showIt()
+    }
+
+    private fun showEdgeCaseErrorDialog() {
+        StaxDialog(requireActivity())
+                .setDialogMessage(getString(R.string.edge_case_bounty_email_error))
+                .setPosButton(R.string.btn_ok, null)
+                .showIt()
+    }
+
+    private fun validates(): Boolean {
+        if (emailInput!!.text == null) return false
+        val email = emailInput!!.text.replace(" ", "")
+        return email.matches("(?:[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])".toRegex())
+    }
+
+    fun observeEmailResult() {
+        viewModel.bountyEmailLiveData.observe(viewLifecycleOwner, { responseMap ->
+            val entry = responseMap.entries.iterator().next()
+            val responseCode = entry.key
+            val message = entry.value
+            if (responseCode in 200..299) saveAndContinue() else {
+                logErrorAndReportToFirebase(TAG, message!!, null)
+                if (isAdded && networkMonitor.isNetworkConnected) showEdgeCaseErrorDialog() else setEmailError()
+            }
+        })
+    }
+
+    private fun setEmailError() {
+        logAnalyticsEvent(getString(R.string.bounty_email_err, getString(R.string.bounty_api_internet_error)), requireContext())
+        emailInput!!.isEnabled = true
+        emailInput!!.setState(getString(R.string.bounty_api_internet_error), AbstractStatefulInput.ERROR)
+    }
+
+    private fun saveAndContinue() {
+        logAnalyticsEvent(getString(R.string.bounty_email_success), requireContext())
+        saveString(BountyActivity.EMAIL_KEY, emailInput!!.text, requireActivity())
+        NavHostFragment.findNavController(this).navigate(R.id.action_bountyEmailFragment_to_bountyListFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (dialog != null && dialog!!.isShowing) dialog!!.dismiss()
+        binding = null
+    }
+
+    companion object {
+        private const val TAG = "BountyEmailFragment"
+    }
+}
---
+++ app/src/main/java/com/hover/stax/bounties/BountyViewModel.kt
@@ -0,0 +1,133 @@
+package com.hover.stax.bounties
+
+import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.lifecycle.*
+import androidx.lifecycle.Observer
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.hover.sdk.actions.HoverAction
+import com.hover.sdk.api.Hover
+import com.hover.sdk.sims.SimInfo
+import com.hover.stax.channels.Channel
+import com.hover.stax.countries.CountryAdapter
+import com.hover.stax.database.DatabaseRepo
+import com.hover.stax.transactions.StaxTransaction
+import com.hover.stax.utils.Utils.getPackage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.java.KoinJavaComponent.get
+import java.util.*
+
+class BountyViewModel(application: Application) : AndroidViewModel(application) {
+    private val repo = get(DatabaseRepo::class.java)
+    @JvmField
+    var country: String = CountryAdapter.codeRepresentingAllCountries()
+
+    val actions: LiveData<List<HoverAction>>
+    val channels: LiveData<List<Channel>>
+    val transactions: LiveData<List<StaxTransaction>>
+    private val filteredBountyChannels: MutableLiveData<List<Channel>?>
+    private val bountyList = MediatorLiveData<List<Bounty>>()
+    private var sims: MutableLiveData<List<SimInfo>?>? = null
+    val bountyEmailLiveData : MutableLiveData<Map<Int, String?>> = MutableLiveData()
+
+
+     fun uploadBountyUser(email:String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = BountyEmailNetworking(getApplication()).uploadBountyUser(email)
+            bountyEmailLiveData.postValue(result)
+        }
+    }
+
+    private fun loadSims() {
+        if (sims == null) {
+            sims = MutableLiveData()
+        }
+        Thread { sims!!.postValue(repo.presentSims) }.start()
+        LocalBroadcastManager.getInstance(getApplication())
+                .registerReceiver(simReceiver, IntentFilter(getPackage(getApplication()) + ".NEW_SIM_INFO_ACTION"))
+        Hover.updateSimInfo(getApplication())
+    }
+
+    private val simReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            Thread { sims!!.postValue(repo.presentSims) }.start()
+        }
+    }
+
+    fun isSimPresent(b: Bounty): Boolean {
+        if (sims!!.value == null || sims!!.value!!.isEmpty()) return false
+        for (sim in sims!!.value!!) {
+            for (i in 0 until b.action.hni_list.length()) if (b.action.hni_list.optString(i) == sim.osReportedHni) return true
+        }
+        return false
+    }
+
+    fun getSims(): LiveData<List<SimInfo>?> {
+        if (sims == null) {
+            sims = MutableLiveData()
+        }
+        return sims!!
+    }
+
+    private fun loadChannels(actions: List<HoverAction>?): LiveData<List<Channel>> {
+        if (actions == null) return MutableLiveData()
+        val ids = getChannelIdArray(actions)
+        return repo.getChannels(ids)
+    }
+
+    val bounties: LiveData<List<Bounty>>
+        get() = bountyList
+
+    fun filterChannels(countryCode: String): LiveData<List<Channel>>? {
+        country = countryCode
+        val actions = actions.value ?: return null
+        return if (countryCode == CountryAdapter.codeRepresentingAllCountries()) loadChannels(actions) else repo.getChannelsByCountry(getChannelIdArray(actions), countryCode)
+    }
+
+    private fun getChannelIdArray(actions: List<HoverAction>): IntArray {
+        val ids = IntArray(actions.size)
+        for (a in actions.indices) ids[a] = actions[a].channel_id
+        return ids
+    }
+
+    private fun makeBountiesIfActions(transactions: List<StaxTransaction>?) {
+        if (actions.value != null && transactions != null) makeBounties(actions.value, transactions)
+    }
+
+    private fun makeBounties(actions: List<HoverAction>?) {
+        if (actions != null) makeBounties(actions, transactions.value)
+    }
+
+    private fun makeBounties(actions: List<HoverAction>?, transactions: List<StaxTransaction>?) {
+        val bounties: MutableList<Bounty> = ArrayList()
+        val transactionsCopy: MutableList<StaxTransaction> = if (transactions == null) ArrayList() else ArrayList(transactions)
+        for (action in actions!!) {
+            val filterTransactions: MutableList<StaxTransaction> = ArrayList()
+            val iter = transactionsCopy.listIterator()
+            while (iter.hasNext()) {
+                val t = iter.next()
+                if (t.action_id == action.public_id) {
+                    filterTransactions.add(t)
+                    iter.remove()
+                }
+            }
+            bounties.add(Bounty(action, filterTransactions))
+        }
+        bountyList.value = bounties
+    }
+
+    init {
+        loadSims()
+        filteredBountyChannels = MutableLiveData()
+        filteredBountyChannels.value = null
+        actions = repo.bountyActions
+        channels = Transformations.switchMap(actions) { actions: List<HoverAction>? -> loadChannels(actions) }
+        transactions = repo.bountyTransactions!!
+        bountyList.addSource(actions) { actions: List<HoverAction>? -> this.makeBounties(actions) }
+        bountyList.addSource(transactions) { transactions: List<StaxTransaction>? -> makeBountiesIfActions(transactions) }
+    }
+}
