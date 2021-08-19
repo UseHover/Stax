715c072ee1be2013ae13706dc422249542109735
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-08-19 20:50:27 +0100

    styling cleanup

--- app/src/main/java/com/hover/stax/transactions/PopupTransactionDetailsFragment.kt
+++ app/src/main/java/com/hover/stax/transactions/PopupTransactionDetailsFragment.kt
@@ -2,7 +2,6 @@ package com.hover.stax.transactions
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/hover/stax/views/StaxTransactionStatusView.kt
+++ app/src/main/java/com/hover/stax/views/StaxTransactionStatusView.kt
@@ -11,9 +11,9 @@ import com.hover.stax.R
 import com.hover.stax.databinding.TransactionStatusLayoutBinding
 import com.hover.stax.transactions.StaxTransaction
 
-open class StaxTransactionStatusView(context: Context, attrs: AttributeSet ) : FrameLayout(context, attrs) {
+open class StaxTransactionStatusView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
     private val binding: TransactionStatusLayoutBinding
-    private var isFlatView : Boolean = false
+    private var isFlatView: Boolean = false
 
     init {
         getAttrs(context, attrs)
@@ -23,18 +23,18 @@ open class StaxTransactionStatusView(context: Context, attrs: AttributeSet ) : F
     private fun getAttrs(context: Context, attrs: AttributeSet) {
         val a = context.theme.obtainStyledAttributes(attrs, R.styleable.StaxTransactionStatusView, 0, 0)
         try {
-           isFlatView = a.getBoolean(R.styleable.StaxTransactionStatusView_isFlatView, false)
+            isFlatView = a.getBoolean(R.styleable.StaxTransactionStatusView_isFlatView, false)
         } finally {
             a.recycle()
         }
     }
 
     private fun fillFromAttrs() {
-        if(isFlatView) binding.notificationCard.makeFlatView()
+        if (isFlatView) binding.notificationCard.makeFlatView()
     }
 
     fun updateInfo(transaction: StaxTransaction) {
-        when(transaction.status){
+        when (transaction.status) {
             Transaction.SUCCEEDED -> setSuccessView(transaction.isRecorded)
             Transaction.PENDING -> setPendingView(transaction.isRecorded)
             Transaction.FAILED -> setFailedView(transaction.isRecorded)
@@ -49,18 +49,18 @@ open class StaxTransactionStatusView(context: Context, attrs: AttributeSet ) : F
             setIcon(R.drawable.ic_success)
             setBackgroundColor(R.color.muted_green)
             setTitle(R.string.confirmed_cardHead)
-            binding.notificationDetail.text = Html.fromHtml(resources.getString(if(isBounty) R.string.flow_done_desc  else R.string.confirmed_desc))
+            binding.notificationDetail.text = Html.fromHtml(resources.getString(if (isBounty) R.string.flow_done_desc else R.string.confirmed_desc))
         }
     }
 
     @SuppressLint("ResourceAsColor")
-    private fun setPendingView(isBounty : Boolean) {
+    private fun setPendingView(isBounty: Boolean) {
         with(binding.notificationCard) {
             setBackButtonVisibility(VISIBLE)
-            setIcon(if(isBounty) R.drawable.ic_warning else R.drawable.ic_info)
-            setBackgroundColor(if(isBounty)  R.color.pending_brown  else R.color.cardDarkBlue)
-            setTitle(if (isBounty)R.string.checking_your_flow  else R.string.pending_cardHead)
-            binding.notificationDetail.text = Html.fromHtml( resources.getString(if(isBounty) R.string.bounty_flow_pending_dialog_msg  else R.string.pending_cardbody))
+            setIcon(if (isBounty) R.drawable.ic_warning else R.drawable.ic_info)
+            setBackgroundColor(if (isBounty) R.color.pending_brown else R.color.cardDarkBlue)
+            setTitle(if (isBounty) R.string.checking_your_flow else R.string.pending_cardHead)
+            binding.notificationDetail.text = Html.fromHtml(resources.getString(if (isBounty) R.string.bounty_flow_pending_dialog_msg else R.string.pending_cardbody))
         }
     }
 
@@ -71,7 +71,7 @@ open class StaxTransactionStatusView(context: Context, attrs: AttributeSet ) : F
             setIcon(R.drawable.ic_info_red)
             setBackgroundColor(R.color.cardDarkRed)
             setTitle(R.string.unsuccessful_cardHead)
-            binding.notificationDetail.text = Html.fromHtml(resources.getString(if (isBounty) R.string.bounty_transaction_failed  else R.string.unsuccessful_desc))
+            binding.notificationDetail.text = Html.fromHtml(resources.getString(if (isBounty) R.string.bounty_transaction_failed else R.string.unsuccessful_desc))
         }
     }
 
