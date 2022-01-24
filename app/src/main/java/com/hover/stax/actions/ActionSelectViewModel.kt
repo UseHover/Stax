3c4276e555d69ae4a9a567b70c140f4352cc58ce
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2022-01-24 15:42:46 +0100

    refractor transfer Fragment

--- app/src/main/java/com/hover/stax/actions/ActionSelectViewModel.kt
+++ app/src/main/java/com/hover/stax/actions/ActionSelectViewModel.kt
@@ -2,17 +2,23 @@ package com.hover.stax.actions
 
 import android.app.Application
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.hover.sdk.actions.HoverAction
 import com.hover.stax.R
+import com.hover.stax.transfers.NonStandardVariable
+import com.hover.stax.views.AbstractStatefulInput
 
 class ActionSelectViewModel(private val application: Application) : ViewModel() {
 
     private val filteredActions = MediatorLiveData<List<HoverAction>>()
     val activeAction = MediatorLiveData<HoverAction>()
+    val nonStandardVariables   =  MutableLiveData<ArrayList<NonStandardVariable>>()
 
     init {
         activeAction.addSource(filteredActions, this::setActiveActionIfOutOfDate)
+        Transformations.map(activeAction,this::setupNonStandardVariables)
     }
 
     private fun setActiveActionIfOutOfDate(actions: List<HoverAction>) {
@@ -28,5 +34,69 @@ class ActionSelectViewModel(private val application: Application) : ViewModel() 
 
     fun errorCheck(): String? {
         return if (activeAction.value == null) application.getString(R.string.action_fielderror) else null
+    }
+
+
+    private fun setupNonStandardVariables(action: HoverAction?) {
+        action?.let {
+            val variableKeys :  List<String> = getNonStandardParams(action)
+            if(variableKeys.isEmpty()) nullifyNonStandardVariables()
+            else initNonStandardVariables(variableKeys)
+        }
+    }
+
+    private fun getNonStandardParams(action: HoverAction) : List<String> {
+        val variableKeys = mutableListOf<String>()
+        action.requiredParams.forEach {
+            if(!isAStandardParam(it)) variableKeys.add(it)
+        }
+        return variableKeys
+    }
+
+    private fun isAStandardParam(param: String): Boolean {
+        return param == HoverAction.PHONE_KEY || param == HoverAction.ACCOUNT_KEY
+                || param == HoverAction.AMOUNT_KEY || param == HoverAction.NOTE_KEY
+                || param == HoverAction.PIN_KEY
+    }
+
+    private fun initNonStandardVariables(variableKeys :  List<String>) {
+        val itemList = ArrayList<NonStandardVariable>()
+        variableKeys.map { itemList.add(NonStandardVariable(it, null)) }
+        nonStandardVariables.postValue(itemList)
+    }
+    private fun nullifyNonStandardVariables() {
+        nonStandardVariables.postValue(null)
+    }
+
+    fun updateNonStandardVariables(nonStandardVariable: NonStandardVariable) {
+        var itemList = nonStandardVariables.value
+        if(itemList == null) itemList = ArrayList()
+
+        itemList.find { it.key == nonStandardVariable.key }
+                ?.let { it.value = nonStandardVariable.value }
+                ?: itemList.add(nonStandardVariable)
+
+        nonStandardVariables.postValue(itemList);
+    }
+
+    fun nonStandardVariablesAnError(): Boolean {
+        with(nonStandardVariables.value) {
+            when {
+                this == null -> return false
+                this.isEmpty() -> return true
+                else -> {
+                    this.forEachIndexed{index, it->
+                        if (it.value == null) it.editTextState = AbstractStatefulInput.ERROR
+                        else {
+                            if (it.value!!.replace(" ".toRegex(), "").isEmpty()) it.editTextState = AbstractStatefulInput.ERROR
+                            else it.editTextState = AbstractStatefulInput.SUCCESS
+                        }
+                    }
+
+                    nonStandardVariables.postValue(this)
+                    return find { it.editTextState == AbstractStatefulInput.ERROR } != null
+                }
+            }
+        }
     }
 }
