92fc209adfb90e0cb78f9d3bbb96a2b035fcd427
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2022-04-13 23:05:09 +0100

    resolved context issue

--- app/src/main/java/com/hover/stax/views/StaxInfoTextView.kt
+++ app/src/main/java/com/hover/stax/views/StaxInfoTextView.kt
@@ -6,10 +6,10 @@ import android.view.LayoutInflater
 import android.widget.FrameLayout
 import com.hover.stax.R
 import com.hover.stax.databinding.InfoTextLayoutBinding
-import com.hover.stax.utils.Utils
 
 class StaxInfoTextView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
-	var binding = InfoTextLayoutBinding.inflate(LayoutInflater.from(context), this, true)
+	private val inflater = LayoutInflater.from(context)
+	var binding = InfoTextLayoutBinding.inflate(inflater, this, true)
 	private var infoTitle : String = ""
 	private var infoContent : String = ""
 	private var textValue: String = ""
@@ -33,14 +33,12 @@ class StaxInfoTextView(context: Context, attrs: AttributeSet) : FrameLayout(cont
 	private fun fillAttr() {
 		binding.textId.text = textValue
 		binding.root.setOnClickListener {
-			Utils.getActivity(context)?.let {
-				StaxDialog(it)
+				StaxDialog(inflater)
 					.setDialogIcon(R.drawable.info_offwhite)
 					.setDialogTitle(infoTitle)
 					.setDialogMessage(infoContent)
 					.setPosButton(R.string.btn_ok, null)
 					.showIt()
-			}
 		}
 	}
 }
