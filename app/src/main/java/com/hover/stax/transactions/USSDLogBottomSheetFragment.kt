7dffd09985949ae3b180f2c1d64e5bc70186c639
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2022-04-15 01:45:39 +0100

    further updates to match design

--- app/src/main/java/com/hover/stax/transactions/USSDLogBottomSheetFragment.kt
+++ app/src/main/java/com/hover/stax/transactions/USSDLogBottomSheetFragment.kt
@@ -29,6 +29,7 @@ class USSDLogBottomSheetFragment: BottomSheetDialogFragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+		binding.closeButton.setOnClickListener { this.dismiss() }
 		setCardTitle()
 		createUSSDMessagesRecyclerView()
 		createSmsMessagesRecyclerView()
