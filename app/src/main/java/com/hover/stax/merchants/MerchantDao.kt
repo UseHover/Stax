8649d7226c7a14ff1e4e1dd83e8a6cfec5fed10c
kutalek <davkutalek@gmail.com>
2022-06-01 16:20:05 -0700

    ordering fix

--- app/src/main/java/com/hover/stax/merchants/MerchantDao.kt
+++ app/src/main/java/com/hover/stax/merchants/MerchantDao.kt
@@ -6,16 +6,16 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MerchantDao {
-	@get:Query("SELECT * FROM merchants ORDER BY till_no ASC")
+	@get:Query("SELECT * FROM merchants ORDER BY last_used_timestamp ASC")
 	val all: LiveData<List<Merchant>>
 
-	@Query("SELECT * FROM merchants WHERE till_no = :till AND channelId = :channelId ORDER BY till_no ASC")
+	@Query("SELECT * FROM merchants WHERE till_no = :till AND channelId = :channelId LIMIT 1")
 	fun getMerchantsByNo(till: String, channelId: Int): Merchant?
 
-	@Query("SELECT * FROM merchants WHERE till_no = :till AND channelId = :channelId ORDER BY till_no ASC")
+	@Query("SELECT * FROM merchants WHERE till_no = :till AND channelId = :channelId LIMIT 1")
 	fun getLiveMerchantsByNo(till: String, channelId: Int): LiveData<Merchant?>
 
-	@Query("SELECT * FROM merchants WHERE id = :id ORDER BY till_no ASC")
+	@Query("SELECT * FROM merchants WHERE id = :id LIMIT 1")
 	fun getMerchant(id: Int): Merchant?
 
 	@Insert(onConflict = OnConflictStrategy.IGNORE)
