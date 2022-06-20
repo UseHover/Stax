42e725c61a5a3c823bd69449e42896030b7b0190
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2022-06-20 15:42:01 +0100

    remove unused codes

--- app/src/main/java/com/hover/stax/views/StaxCardView.kt
+++ app/src/main/java/com/hover/stax/views/StaxCardView.kt
@@ -43,7 +43,6 @@ open class StaxCardView(context: Context, attrs: AttributeSet) : FrameLayout(con
     private fun makeFlatView() {
         val zero = 0
         binding.cardView.cardElevation = zero.toFloat()
-        //binding.cardView.radius = zero.toFloat()
         removeCardMargin()
     }
 
