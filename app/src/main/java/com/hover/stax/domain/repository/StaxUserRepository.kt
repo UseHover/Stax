6d8bd0a27f80febdfe90edd045db590f65e86b43
Alex Kombo <alexkombo@gmail.com>
2022-08-25 15:58:57 +0300

    completes refactor of loginViewModel to use retrofit client and new api methods

--- app/src/main/java/com/hover/stax/domain/repository/StaxUserRepository.kt
+++ app/src/main/java/com/hover/stax/domain/repository/StaxUserRepository.kt
@@ -1,12 +1,20 @@
 package com.hover.stax.domain.repository
 
-import com.hover.stax.data.remote.dto.UserRequestDto
+import com.hover.stax.data.remote.dto.UserUpdateDto
+import com.hover.stax.data.remote.dto.UserUploadDto
 import com.hover.stax.user.StaxUser
+import kotlinx.coroutines.flow.Flow
 
 interface StaxUserRepository {
 
-    suspend fun uploadUser(user: UserRequestDto): StaxUser
+    suspend fun uploadUser(userUploadDto: UserUploadDto): StaxUser
 
-    suspend fun updateUser(email: String, user: UserRequestDto): StaxUser
+    suspend fun updateUser(email: String, userUpdateDto: UserUpdateDto): StaxUser
+
+    val staxUser: Flow<StaxUser>
+
+    suspend fun saveUser(user: StaxUser)
+
+    suspend fun deleteUser(user: StaxUser)
 
 }
--- app/src/main/java/com/hover/stax/domain/use_case/stax_user/StaxUserUseCase.kt
+++ app/src/main/java/com/hover/stax/domain/use_case/stax_user/StaxUserUseCase.kt
@@ -1,33 +1,42 @@
 package com.hover.stax.domain.use_case.stax_user
 
-import com.hover.stax.data.remote.dto.UserRequestDto
+import com.hover.stax.data.remote.dto.UserUpdateDto
+import com.hover.stax.data.remote.dto.UserUploadDto
 import com.hover.stax.domain.model.Resource
 import com.hover.stax.domain.repository.StaxUserRepository
 import com.hover.stax.user.StaxUser
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 
 class StaxUserUseCase(private val staxUserRepository: StaxUserRepository) {
 
-    fun uploadUser(userRequestDto: UserRequestDto): Flow<Resource<StaxUser>> = flow {
+    val user = staxUserRepository.staxUser
+
+    fun uploadUser(userUploadDto: UserUploadDto): Flow<Resource<StaxUser>> = flow {
         try {
             emit(Resource.Loading())
 
-            val staxUser = staxUserRepository.uploadUser(userRequestDto)
+            val staxUser = staxUserRepository.uploadUser(userUploadDto)
+
             emit(Resource.Success(staxUser))
         } catch (e: Exception) {
+            Timber.e(e)
             emit(Resource.Error("Error uploading user. Please try again later."))
         }
     }
 
-    fun updateUser(email: String, userRequestDto: UserRequestDto): Flow<Resource<StaxUser>> = flow {
+    fun updateUser(email: String, userUpdateDto: UserUpdateDto): Flow<Resource<StaxUser>> = flow {
         try {
             emit(Resource.Loading())
 
-            val staxUser = staxUserRepository.updateUser(email, userRequestDto)
+            val staxUser = staxUserRepository.updateUser(email, userUpdateDto)
+
             emit(Resource.Success(staxUser))
         } catch (e: Exception) {
             emit(Resource.Error("Error updating user details. Please try again later."))
         }
     }
+
+    suspend fun deleteUser(user: StaxUser) = staxUserRepository.deleteUser(user)
 }
