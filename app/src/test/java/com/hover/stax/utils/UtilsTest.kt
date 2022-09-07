e0e6bdfbe7736b5798b9a991dc9f095bfbd6ac5e
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2022-09-07 20:48:11 +0100

    added more test to format amount

--- app/src/test/java/com/hover/stax/utils/UtilsTest.kt
+++ app/src/test/java/com/hover/stax/utils/UtilsTest.kt
@@ -7,7 +7,19 @@ import org.junit.Test
 class UtilsTest {
 	@Test
 	fun formatAmount_UsingADouble_IsCorrect() {
-		val result = Utils.formatAmount(43.98)
-		Assert.assertEquals("43.98", result)
+		val variation1Input  = 43.0
+		val variation2Input = 43.1
+		val variation3Input = 43.98
+		val variation4Input = 43.986
+
+		val variation1Result = Utils.formatAmount(variation1Input)
+		val variation2Result = Utils.formatAmount(variation2Input)
+		val variation3Result = Utils.formatAmount(variation3Input)
+		val variation4Result = Utils.formatAmount(variation4Input)
+
+		Assert.assertEquals("43.00", variation1Result)
+		Assert.assertEquals("43.10", variation2Result)
+		Assert.assertEquals(variation3Input.toString(), variation3Result)
+		Assert.assertEquals(variation4Input.toString(), variation4Result)
 	}
 }
