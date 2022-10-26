44edfcdf81b5e00718d2acc4381403767d0e3c1e
kutalek <davkutalek@gmail.com>
2022-10-26 12:15:07 -0700

    Update to latest SDK, target API level 33

--- app/src/main/java/com/hover/stax/presentation/sims/SimScreen.kt
+++ app/src/main/java/com/hover/stax/presentation/sims/SimScreen.kt
@@ -1,6 +1,5 @@
 package com.hover.stax.presentation.sims
 
-import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,7 +9,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
@@ -18,20 +16,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import com.hover.sdk.sims.SimInfo
 import com.hover.stax.R
 import com.hover.stax.domain.model.Account
 import com.hover.stax.domain.use_case.sims.SimWithAccount
 import com.hover.stax.permissions.PermissionUtils
-import com.hover.stax.presentation.home.BalanceTapListener
 import com.hover.stax.presentation.home.components.TopBar
 import com.hover.stax.presentation.sims.components.LinkSimCard
 import com.hover.stax.presentation.sims.components.SampleSimInfoProvider
 import com.hover.stax.presentation.sims.components.SimItem
 import com.hover.stax.ui.theme.*
-import com.hover.stax.utils.network.NetworkMonitor
 import org.koin.androidx.compose.getViewModel
-import timber.log.Timber
 
 @Composable
 fun SimScreen(
