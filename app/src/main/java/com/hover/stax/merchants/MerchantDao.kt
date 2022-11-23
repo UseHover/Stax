3a5a47c42dac4e10187b31dd9092dca74d1716aa
Juma Allan <allanjuma@gmail.com>
2022-11-23 23:32:05 +0300

    remove unused code

--- app/src/main/java/com/hover/stax/merchants/MerchantDao.kt
+++ app/src/main/java/com/hover/stax/merchants/MerchantDao.kt
@@ -20,21 +20,6 @@ interface MerchantDao : BaseDao<Merchant> {
 	@Query("SELECT * FROM merchants WHERE id = :id LIMIT 1")
 	fun getMerchant(id: Int): Merchant?
 
-//	@Insert(onConflict = OnConflictStrategy.IGNORE)
-//	fun insert(vararg merchant: Merchant?)
-
-//	@Insert(onConflict = OnConflictStrategy.REPLACE)
-//	fun insert(merchant: Merchant)
-
-//	@Update
-//	fun update(merchant: Merchant)
-//
-//	@Update
-//	fun update(merchant: List<Merchant>)
-//
-//	@Delete
-//	fun delete(merchant: Merchant)
-
 	@Query("DELETE FROM merchants")
 	fun deleteAll()
 }
