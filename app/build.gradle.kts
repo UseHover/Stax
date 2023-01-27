9c34dc6d1a5dbc09f4945e061d176c656d8066fe
Anselmo Alexandre <anselmo@snappmobile.io>
2023-01-27 21:32:34 +0200

    Updated kotlin-stdlib to avoid gradle duplications compile issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,16 @@ plugins {
     id("kotlinx-serialization")
 }
 
+configurations.all {
+    resolutionStrategy {
+        eachDependency {
+            if ((requested.group == "org.jetbrains.kotlin") && (requested.name.startsWith("kotlin-stdlib"))) {
+                useVersion("1.8.0")
+            }
+        }
+    }
+}
+
 android {
 
     namespace = "com.hover.stax"
