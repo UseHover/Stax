1792e01855f4f1758b5df32d94b4745dffac6b51
Anselmo Alexandre <anselmo@snappmobile.io>
2023-02-08 18:23:12 +0200

    Provided SimInfo

--- core/storage/src/main/java/com/hover/stax/storage/di/DatabaseModule.kt
+++ core/storage/src/main/java/com/hover/stax/storage/di/DatabaseModule.kt
@@ -1,7 +1,11 @@
 package com.hover.stax.storage.di
 
+import com.hover.sdk.database.HoverRoomDatabase
+import com.hover.sdk.sims.SimInfoDao
 import com.hover.stax.storage.channel.repository.ChannelRepository
 import com.hover.stax.storage.channel.repository.ChannelRepositoryImpl
+import com.hover.stax.storage.sim.SimInfoRepository
+import com.hover.stax.storage.sim.SimInfoRepositoryImpl
 import com.hover.stax.storage.user.repository.StaxUserRepositoryImpl
 import com.hover.stax.storage.user.repository.StaxUserRepository
 import org.koin.core.module.dsl.bind
@@ -13,5 +17,8 @@ object DatabaseModule {
     val repository = module {
         singleOf(::StaxUserRepositoryImpl) { bind<StaxUserRepository>() }
         singleOf(::ChannelRepositoryImpl) { bind<ChannelRepository>() }
+
+        single<SimInfoDao> { HoverRoomDatabase.getInstance(get()).simDao() }
+        singleOf(::SimInfoRepositoryImpl) { bind<SimInfoRepository>() }
     }
 }
