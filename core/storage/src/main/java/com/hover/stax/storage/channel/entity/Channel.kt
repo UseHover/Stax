229c99807e7f5c96029b66a62189fa597741756f
Anselmo Alexandre <anselmo@snappmobile.io>
2023-02-08 16:39:33 +0200

    Migrated Channel from app to storage module converted from java to kotlin entity data class

--- core/storage/src/main/java/com/hover/stax/storage/channel/entity/Channel.kt
+++ core/storage/src/main/java/com/hover/stax/storage/channel/entity/Channel.kt
@@ -1,16 +1,47 @@
 package com.hover.stax.storage.channel.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-private enum class ChannelTypes(val type:String){
-    BANK(type = "bank"),
-    TELECOM(type = "telecom"),
-    MOBILE_MONEY(type = "mmo")
-}
-
-
 @Entity(tableName = "channels")
-data class ChannelNow(
-    @PrimaryKey(autoGenerate = false) val id:Int
+data class Channel(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "country_alpha2")
+    val countryAlpha2: String,
+
+    @ColumnInfo(name = "root_code")
+    val rootCode: String,
+
+    @ColumnInfo(name = "currency")
+    val currency: String,
+
+    @ColumnInfo(name = "hni_list")
+    val hniList: String,
+
+    @ColumnInfo(name = "logo_url")
+    val logoUrl: String,
+
+    @ColumnInfo(name = "institution_id")
+    val institutionId: Int,
+
+    @ColumnInfo(name = "primary_color_hex")
+    val primaryColorHex: String,
+
+    @ColumnInfo(name = "published")
+    val published: Boolean = false,
+
+    @ColumnInfo(name = "secondary_color_hex")
+    val secondaryColorHex: String,
+
+    @ColumnInfo(name = "institution_type")
+    val institutionType: String = ChannelTypes.BANK.type,
+
+    @ColumnInfo(name = "isFavorite")
+    val isFavorite: Boolean = false
 )
