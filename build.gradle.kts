dd7b08102639915adbeb43218026ccb8d4b7088d
Juma Allan <allanjuma@gmail.com>
2023-02-09 02:21:14 +0300

    updated spotless config for xml and kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,5 +50,17 @@ subprojects {
                 "^(package|object|import|interface)"
             )
         }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            // Look for the first line that doesn't have a block comment (assumed to be the license)
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+            // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
+            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+        }
     }
 }
