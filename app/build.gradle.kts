b6e5917cb2b5180d2f52b6b9e4692accb4ec67d7
kutalek <davkutalek@gmail.com>
2023-03-06 09:19:37 -0800

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ configurations.all {
 }
 
 group = "com.hover"
-version = "1.19.7"
+version = "1.19.8"
 
 android {
 
@@ -53,7 +53,7 @@ android {
         applicationId = "com.hover.stax"
         minSdk = 21
         targetSdk = 33
-        versionCode = 217
+        versionCode = 218
         versionName = project.version.toString()
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
