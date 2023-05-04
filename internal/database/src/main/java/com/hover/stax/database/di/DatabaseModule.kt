8d8bff0f3451fb35dcf421ffa511f87568a3886f
Juma Allan <allanjuma@gmail.com>
2023-05-04 03:21:56 +0300

    setting up stax database

---
+++ internal/database/src/main/java/com/hover/stax/database/di/DatabaseModule.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Stax
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hover.stax.database.di
+
+import android.content.Context
+import androidx.room.Room
+import com.hover.stax.database.StaxDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providesStaxDatabase(
+        @ApplicationContext context: Context,
+    ): StaxDatabase = Room.databaseBuilder(
+        context,
+        StaxDatabase::class.java,
+        "stax-database",
+    ).build()
+}
